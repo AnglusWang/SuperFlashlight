de6d52636fcf659d7cedaa7b4354a4f2ad826711
AnglusWang <AnglusWang@163.com>
2016-06-20 23:02:39 +0800

    Update

--- app/src/main/java/com/example/angluswang/superflashlight/BaseActivity.java
+++ app/src/main/java/com/example/angluswang/superflashlight/BaseActivity.java
@@ -2,6 +2,7 @@ package com.example.angluswang.superflashlight;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 /**
  * Created by Jeson on 2016/5/30.
@@ -10,9 +11,13 @@ import android.os.Bundle;
 
 public class BaseActivity extends Activity {
 
+    private ImageView imgFlashlight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgFlashlight = (ImageView) findViewById(R.id.img_flashlight);
     }
 }
--- app/src/main/java/com/example/angluswang/superflashlight/FlashActivity.java
+++ app/src/main/java/com/example/angluswang/superflashlight/FlashActivity.java
@@ -1,5 +1,10 @@
 package com.example.angluswang.superflashlight;
 
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 /**
  * Created by Jeson on 2016/5/30.
  *
@@ -7,4 +12,18 @@ package com.example.angluswang.superflashlight;
 
 public class FlashActivity extends BaseActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void openflashlight (View view) {
+        if (!getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA_FLASH)) {
+            Toast.makeText(this, "没有闪光灯",
+                    Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "可以使用闪光灯", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
